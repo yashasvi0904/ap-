77ce8c9f895f1c65f9ef5ea9daf9017d7ab82992
TanushChawla18 <tanushchawla16@gmail.com>
2024-09-17 20:32:23 +0530

    Updated professor portal complete

--- Professor.java
+++ Professor.java
@@ -55,15 +55,15 @@ import java.util.Scanner;
         }
 
         public void display() {
-            System.out.println(courseName + " " + syllabus + " " + classTimings + " " + credits + " " + prerequisites
-                    + " " + enrollmentLimit + " " + officeHours);
+            System.out.println("course Name"+ courseName + " "+"Syallbus" + syllabus + " "+"Class Timings" + classTimings + " "+"Credits" + credits + " "+"Prerequisites" + prerequisites
+                    + " "+ "Enrollment Limit" + enrollmentLimit + " "+ "Office Hours" + officeHours);
 
         }
     }
     class Student {
-        private String name;
-        private String academicStanding;
-        private String contactDetails;
+        public String name;
+        public String academicStanding;
+        public String contactDetails;
 
         public Student(String name, String academicStanding, String contactDetails) {
             this.name = name;
@@ -112,12 +112,82 @@ import java.util.Scanner;
                 System.exit(0);
             }
         }
-    }
+    
 
-    public static void editCourse() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the course name");
-        String courseName = sc.nextLine();
+        public static void editCourse() {
+            Scanner sc = new Scanner(System.in);
+            for (int i = 0; i < courses.size(); i++) {
+                System.out.println(courses.get(i).courseName());
+            }
+            System.out.println("Enter the course name");
+
+            String courseName = sc.nextLine();
+
+            for (int i = 0; i < courses.size(); i++) {
+                if (courses.get(i).courseName().equals(courseName)) {
+                    System.out.println("Enter the option");
+                    System.out.println("1. Edit Syallbus");
+                    System.out.println("2. Edit Class Timings");
+                    System.out.println("3. Edit Credits");
+                    System.out.println("4. Edit Prerequisites");
+                    System.out.println("5. Edit Enrollment Limit");
+                    System.out.println("6. Edit Office Hours");
+                    int option = sc.nextInt();
+                    sc.nextLine();
+                    if (option == 1) {
+                        System.out.println("Enter the Syallbus");
+                        String syllabus = sc.nextLine();
+                        courses.get(i).syallbus_edit(syllabus);
+                    }
+
+                    if (option == 2) {
+                        System.out.println("Enter the Class Timings");
+                        String classTimings = sc.nextLine();
+                        courses.get(i).classTimings_edit(classTimings);
+                    }
+
+                    if (option == 3) {
+                        System.out.println("Enter the Credits");
+                        int credits = sc.nextInt();
+                        courses.get(i).credits_edit(credits);
+                    }
+
+                    if (option == 4) {
+                        System.out.println("Enter the Prerequisites");
+                        String prerequisites = sc.nextLine();
+                        courses.get(i).prerequisites_edit(prerequisites);
+                    }
+
+                    if (option == 5) {
+                        System.out.println("Enter the Enrollment Limit");
+                        int enrollmentLimit = sc.nextInt();
+                        courses.get(i).enrollmentLimit_edit(enrollmentLimit);
+                    }
+
+                    if (option == 6) {
+                        System.out.println("Enter the Office Hours");
+                        String officeHours = sc.nextLine();
+                        courses.get(i).officeHours_edit(officeHours);
+                    }
+
+                    sc.close();
+                    courses.get(i).display();
+                    break;
+                }
+            }
+
+        }
+
+        public static void enrolledStudents() {
+            for (int i = 0; i < students.size(); i++) {
+                System.out.println(students.get(i).name + " " + students.get(i).academicStanding + " "
+                        + students.get(i).contactDetails);
+            }
+            System.out.println("Thanks for using the professor portal");
+
+
+        }
+}
         
         
 
